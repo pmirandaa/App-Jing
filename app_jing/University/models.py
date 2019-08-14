40f87b30efe4eb3dc4ff69b8201d0ecc11c5aa30
jzambraf <lapnis@hotmail.com>
2019-08-14 03:33:55 -0400

    Edit and Captain
    All edit in administration, action feedback for administration and teams, and edit with captain selector in teams

--- app_jing/University/models.py
+++ app_jing/University/models.py
@@ -7,7 +7,8 @@ class University(models.Model):
     name = models.CharField(max_length=100)
     city = models.CharField(max_length=30)
     overall_score = models.IntegerField(default=0)
-    logo = models.ImageField(upload_to='university/logo/', blank=True, null=True)
+    logo = models.ImageField(
+        upload_to='university/logo/', blank=True, null=True)
     map = models.ImageField(upload_to='university/map/', blank=True, null=True)
     short_name = models.CharField(max_length=10)
 
