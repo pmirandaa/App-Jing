de944a254fcf28387f0e6aa5d70ee06fefcafefd
scisneros <sebastian.cisneros384@gmail.com>
2022-07-21 10:23:31 -0400

    Change Routes and CSSTransition hierarchy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,45 @@
 import styles from "./App.module.css";
 import { useState } from "react";
-import { Outlet, useLocation } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Navigate,
+  useLocation
+} from "react-router-dom";
 
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import NavBar from "components/navbar/NavBar";
 import { EventContext } from "contexts/EventContext";
 
+import News from 'pages/News';
+import Messages from 'pages/Messages';
+import Info from 'pages/Info';
+import Matches from 'pages/Matches';
+import Teams from 'pages/Teams';
+import Admin from 'pages/Admin';
+
 function App() {
   const [event, setEvent] = useState(1);
-  const location = useLocation();  
-  
+  const location = useLocation();
+
   return (
-    <EventContext.Provider value={{event, setEvent}}>
+    <EventContext.Provider value={{ event, setEvent }}>
       <div className={styles.root}>
         <NavBar />
         <TransitionGroup component={null} exit={false}>
           <CSSTransition key={location.pathname} classNames="fade" timeout={0}>
-            <Outlet />
+            <Routes location={location}>
+              <Route index element={<News />} />
+              <Route path="personas" element={<Info />} />
+              <Route path="partidos" element={<Matches />} />
+              <Route path="equipos" element={<Teams />} />
+              <Route path="mensajes" element={<Messages />} />
+              <Route path="administracion" element={<Admin />} />
+
+              <Route path="/hola" element={<Teams />} />
+              <Route path="*" element={<Navigate to="/" replace={true} />} />
+            </Routes>
           </CSSTransition>
         </TransitionGroup>
       </div>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,38 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import 'index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from 'App';
 import reportWebVitals from 'reportWebVitals';
-import News from 'pages/News';
-import Messages from 'pages/Messages';
-import Info from 'pages/Info';
-import Matches from 'pages/Matches';
-import Teams from 'pages/Teams';
-import Admin from 'pages/Admin';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} >
-          <Route index element={<News />} />
-          <Route path="personas" element={<Info/>} />
-          <Route path="partidos" element={<Matches/>} />
-          <Route path="equipos" element={<Teams/>} />
-          <Route path="mensajes" element={<Messages/>} />
-          <Route path="administracion" element={<Admin/>} />
-        </Route>
-        
-        <Route path="/hola" element={<Teams />} />
-        <Route path="*" element={<Navigate to="/" replace={true} />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
