a5e68d41e27c43d4274081bc57ed434ad20a466c
scisneros <sebastian.cisneros384@gmail.com>
2022-12-22 19:30:57 -0300

    Server settings based on environment variables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,11 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k-h$6g@5zstx+qfl+q_7)r$olnm&j3b2z+ig7pymf==afpgtzv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('JING_ALLOWED_HOSTS', '').split()
 
 
 # Application definition
@@ -142,9 +142,9 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-]
+CORS_ORIGIN_WHITELIST = os.getenv('JING_CORS_ORIGIN_WHITELIST', '').split() or ['http://localhost:3000']
+
+# Django REST Framework
 
 REST_FRAMEWORK = {
     #'DEFAULT_PERMISSION_CLASSES': [
