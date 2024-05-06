ce05cdb923c5beaa00f2e5bea5e2a6d2338c6e0a
Dapsy420 <81329159+Dapsy420@users.noreply.github.com>
2024-05-06 19:04:22 -0400

    cambios al modelo de mensajes
    Se cambia el modelo de mensajes para añadir canales de chat

--- backend/Message/models.py
+++ backend/Message/models.py
@@ -6,7 +6,7 @@ from Person.models import Person
 class Message(models.Model):
     sender = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='sender')
     reciever = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='reciever')
-    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    #event = models.ForeignKey(Event, on_delete=models.CASCADE)
     subject = models.CharField(max_length=100)
     body = models.CharField(max_length=1000)
     is_read = models.BooleanField(default=False)
@@ -15,3 +15,12 @@ class Message(models.Model):
 
     def __str__(self):
         return f'{self.sender} --> {self.reciever} -- {self.subject}'
+    
+class Chanel(models.Model):
+    name= models.CharField(max_length=20)
+    event= models.ForeignKey(Event, on_delete=models.CASCADE)
+    is_active= models.BooleanField(default=False)
+
+class ChanelUser(models.Model):
+    user= models.ForeignKey(Person, on_delete=models.CASCADE)
+    chanel= models.ForeignKey(Chanel,on_delete=models.CASCADE )
